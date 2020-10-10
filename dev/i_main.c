e77e357b7782feb0c7fc0d1bbc5d93dd76bc5f30
StevePro7 <steveprozune@hotmail.com>
2020-10-10 15:04:20 +0100

    no message

--- dev/i_main.c
+++ dev/i_main.c
@@ -1,5 +1,21 @@
-int main( int argc, char** argv )
+static const char
+rcsid[] = "$Id: i_main.c,v 1.4 1997/02/03 22:45:10 b1 Exp $";
+
+
+
+#include "doomdef.h"
+#include "m_argv.h"
+
+
+int
+main
+( int		argc,
+  char**	argv )
 {
-	int i = 7;
+	myargc = argc;
+	myargv = argv;
+
+	//D_DoomMain();
+
 	return 0;
 }
