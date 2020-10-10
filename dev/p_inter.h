0f10728c18071904ab62207e2894805271eadb9e
StevePro7 <steveprozune@hotmail.com>
2020-10-10 16:30:59 +0100

    no message

--- dev/p_inter.h
+++ dev/p_inter.h
@@ -14,51 +14,28 @@
 // FITNESS FOR A PARTICULAR PURPOSE. See the DOOM Source Code License
 // for more details.
 //
-// $Log:$
-//
 // DESCRIPTION:
-//	System specific interface stuff.
+//
 //
 //-----------------------------------------------------------------------------
 
 
-#ifndef __D_MAIN__
-#define __D_MAIN__
+#ifndef __P_INTER__
+#define __P_INTER__
 
-#include "d_event.h"
 
 #ifdef __GNUG__
 #pragma interface
 #endif
 
 
-
-#define MAXWADFILES             20
-extern char*		wadfiles[ MAXWADFILES ];
-
-void D_AddFile( char *file );
+boolean	P_GivePower( player_t*, int );
 
 
-
-//
-// D_DoomMain()
-// Not a globally visible function, just included for source reference,
-// calls all startup code, parses command line options.
-// If not overrided by user input, calls N_AdvanceDemo.
-//
-void D_DoomMain( void );
-
-// Called by IO functions when input is detected.
-void D_PostEvent( event_t* ev );
-
-
-
-//
-// BASE LEVEL
-//
-void D_PageTicker( void );
-void D_PageDrawer( void );
-void D_AdvanceDemo( void );
-void D_StartTitle( void );
 
 #endif
+//-----------------------------------------------------------------------------
+//
+// $Log:$
+//
+//-----------------------------------------------------------------------------
