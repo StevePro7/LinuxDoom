e9e74d920090c5de3e384c7a3794307276364c01
StevePro7 <steveprozune@hotmail.com>
2020-10-11 10:58:29 +0100

    no message

--- dev/i_system.c
+++ dev/i_system.c
@@ -1,65 +1,77 @@
+// Emacs style mode select   -*- C++ -*- 
+//-----------------------------------------------------------------------------
+//
+// $Id:$
+//
+// Copyright (C) 1993-1996 by id Software, Inc.
+//
+// This source is available for distribution and/or modification
+// only under the terms of the DOOM Source Code License as
+// published by id Software. All rights reserved.
+//
+// The source is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// FITNESS FOR A PARTICULAR PURPOSE. See the DOOM Source Code License
+// for more details.
+//
+// $Log:$
+//
+// DESCRIPTION:
+//
+//-----------------------------------------------------------------------------
+
+static const char
+rcsid[] = "$Id: m_bbox.c,v 1.1 1997/02/03 22:45:10 b1 Exp $";
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <stdarg.h>
+//#include <sys/time.h>
+//#include <unistd.h>
+
+#include "doomdef.h"
+#include "m_misc.h"
+#include "i_video.h"
+#include "i_sound.h"
+
+#include "d_net.h"
+#include "g_game.h"
 
 #ifdef __GNUG__
 #pragma implementation "i_system.h"
 #endif
 #include "i_system.h"
 
-#include <stdio.h>
-#include <stdlib.h>
 
-//
-// I_Error
-//
-extern boolean demorecording;
+
 
 int	mb_used = 6;
 
-void I_Error( char *error, ... )
+
+void
+I_Tactile
+( int	on,
+	int	off,
+	int	total )
 {
-	//va_list	argptr;
+	// UNUSED.
+	on = off = total = 0;
+}
 
-	//// Message first.
-	//va_start( argptr, error );
-	//fprintf( stderr, "Error: " );
-	//vfprintf( stderr, error, argptr );
-	//fprintf( stderr, "\n" );
-	//va_end( argptr );
-
-	//fflush( stderr );
-
-	//// Shutdown. Here might be other errors.
-	//if( demorecording )
-	//	G_CheckDemoStatus();
-
-	//D_QuitNetGame();
-	//I_ShutdownGraphics();
-
-	//exit( -1 );
+ticcmd_t	emptycmd;
+ticcmd_t*	I_BaseTiccmd( void )
+{
+	return &emptycmd;
 }
 
 
-//
-// I_GetTime
-// returns time in 1/70th second tics
-//
-int  I_GetTime( void )
+int  I_GetHeapSize( void )
 {
-	// TODO
-	//struct timeval	tp;
-	//struct timezone	tzp;
-	int			newtics;
-	//static int		basetime = 0;
-
-	//gettimeofday( &tp, &tzp );
-	//if( !basetime )
-	//	basetime = tp.tv_sec;
-	//newtics = ( tp.tv_sec - basetime )*TICRATE + tp.tv_usec*TICRATE / 1000000;
-
-	newtics = 0;
-	return newtics;
+	return mb_used * 1024 * 1024;
 }
-
-
 
 byte* I_ZoneBase( int*	size )
 {
@@ -68,12 +80,48 @@ byte* I_ZoneBase( int*	size )
 }
 
 
-// TODO
-byte* I_AllocLow( int length )
+
+//
+// I_GetTime
+// returns time in 1/70th second tics
+//
+int  I_GetTime( void )
 {
-	return NULL;
+	//struct timeval	tp;
+	//struct timezone	tzp;
+	int			newtics = 0;		// stevepro
+	//static int		basetime = 0;
+
+	//gettimeofday( &tp, &tzp );
+	//if( !basetime )
+	//	basetime = tp.tv_sec;
+	//newtics = ( tp.tv_sec - basetime )*TICRATE + tp.tv_usec*TICRATE / 1000000;
+	return newtics;
 }
 
+
+
+//
+// I_Init
+//
+void I_Init( void )
+{
+	I_InitSound();
+	//  I_InitGraphics();
+}
+
+//
+// I_Quit
+//
+void I_Quit( void )
+{
+	D_QuitNetGame();
+	I_ShutdownSound();
+	I_ShutdownMusic();
+	M_SaveDefaults();
+	I_ShutdownGraphics();
+	exit( 0 );
+}
 
 void I_WaitVBL( int count )
 {
@@ -83,8 +131,7 @@ void I_WaitVBL( int count )
 #ifdef SUN
 	sleep( 0 );
 #else
-	// TODO
-	//usleep( count * ( 1000000 / 70 ) );
+	//usleep( count * ( 1000000 / 70 ) );		// stevepro
 #endif
 #endif
 }
@@ -95,4 +142,42 @@ void I_BeginRead( void )
 
 void I_EndRead( void )
 {
+}
+
+byte*	I_AllocLow( int length )
+{
+	byte*	mem;
+
+	mem = ( byte * ) malloc( length );
+	memset( mem, 0, length );
+	return mem;
+}
+
+
+//
+// I_Error
+//
+extern boolean demorecording;
+
+void I_Error( char *error, ... )
+{
+	va_list	argptr;
+
+	// Message first.
+	va_start( argptr, error );
+	fprintf( stderr, "Error: " );
+	vfprintf( stderr, error, argptr );
+	fprintf( stderr, "\n" );
+	va_end( argptr );
+
+	fflush( stderr );
+
+	// Shutdown. Here might be other errors.
+	if( demorecording )
+		G_CheckDemoStatus();
+
+	D_QuitNetGame();
+	I_ShutdownGraphics();
+
+	exit( -1 );
 }
--- dev/m_misc.c
+++ dev/m_misc.c
@@ -1,1 +1,33 @@
 #include "m_misc.h"
+
+
+//
+// M_SaveDefaults
+//
+void M_SaveDefaults( void )
+{
+	// TODO
+	//int		i;
+	//int		v;
+	//FILE*	f;
+
+	//f = fopen( defaultfile, "w" );
+	//if( !f )
+	//	return; // can't write the file, but don't complain
+
+	//for( i = 0; i < numdefaults; i++ )
+	//{
+	//	if( defaults[ i ].defaultvalue > -0xfff
+	//		&& defaults[ i ].defaultvalue < 0xfff )
+	//	{
+	//		v = *defaults[ i ].location;
+	//		fprintf( f, "%s\t\t%i\n", defaults[ i ].name, v );
+	//	}
+	//	else {
+	//		fprintf( f, "%s\t\t\"%s\"\n", defaults[ i ].name,
+	//			*( char ** ) ( defaults[ i ].location ) );
+	//	}
+	//}
+
+	//fclose( f );
+}
